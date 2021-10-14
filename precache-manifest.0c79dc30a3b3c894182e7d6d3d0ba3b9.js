f332d9261afe7c44907dc5c799f88a4ad4a79bb1
Cicero Viana <cicero.viana@hp.com>
2021-10-14 11:13:22 -0300

    Updates

---
+++ precache-manifest.0c79dc30a3b3c894182e7d6d3d0ba3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54761e0076414923b1d331f785a12240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "683e1eb9bf859d6b14ee",
+    "url": "/static/js/2.f8707b5e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.f8707b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4a687baa528540ed7a",
+    "url": "/static/js/main.75690ddc.chunk.js"
+  },
+  {
+    "revision": "c440a3db2a83af8692d4",
+    "url": "/static/js/runtime-main.1d75ce24.js"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "e37b6ce76ce341e72779cce0fd925103",
+    "url": "/static/media/sails.e37b6ce7.svg"
+  }
+]);
